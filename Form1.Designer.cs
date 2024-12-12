2a45ed9e0f0ca03c2943957b0a884a76979278e2
Andy De George (adegeo) <adegeo@microsoft.com>
2024-12-12 12:24:40 -0800

    Initial code

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace IssueTissue
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUrl = new TextBox();
+            btnClean = new Button();
+            splitContainer1 = new SplitContainer();
+            txtIssue = new TextBox();
+            label1 = new Label();
+            btnCopy = new Button();
+            txtOutput = new TextBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            txtUrl.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtUrl.Location = new Point(12, 12);
+            txtUrl.Name = "txtUrl";
+            txtUrl.Size = new Size(578, 23);
+            txtUrl.TabIndex = 0;
+            txtUrl.KeyPress += txtUrl_KeyPress;
+            // 
+            // btnClean
+            // 
+            btnClean.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnClean.Location = new Point(596, 11);
+            btnClean.Name = "btnClean";
+            btnClean.Size = new Size(75, 23);
+            btnClean.TabIndex = 1;
+            btnClean.Text = "Clean";
+            btnClean.UseVisualStyleBackColor = true;
+            btnClean.Click += btnClean_Click;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            splitContainer1.Location = new Point(12, 41);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(txtIssue);
+            splitContainer1.Panel1.Controls.Add(label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(btnCopy);
+            splitContainer1.Panel2.Controls.Add(txtOutput);
+            splitContainer1.Panel2.Controls.Add(label2);
+            splitContainer1.Size = new Size(659, 387);
+            splitContainer1.SplitterDistance = 167;
+            splitContainer1.TabIndex = 2;
+            // 
+            // txtIssue
+            // 
+            txtIssue.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtIssue.Location = new Point(0, 18);
+            txtIssue.Multiline = true;
+            txtIssue.Name = "txtIssue";
+            txtIssue.ReadOnly = true;
+            txtIssue.ScrollBars = ScrollBars.Both;
+            txtIssue.Size = new Size(659, 146);
+            txtIssue.TabIndex = 1;
+            txtIssue.WordWrap = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Issue Text";
+            // 
+            // btnCopy
+            // 
+            btnCopy.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnCopy.Location = new Point(584, 193);
+            btnCopy.Name = "btnCopy";
+            btnCopy.Size = new Size(75, 23);
+            btnCopy.TabIndex = 3;
+            btnCopy.Text = "Copy";
+            btnCopy.UseVisualStyleBackColor = true;
+            btnCopy.Click += btnCopy_Click;
+            // 
+            // txtOutput
+            // 
+            txtOutput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtOutput.Location = new Point(0, 18);
+            txtOutput.Multiline = true;
+            txtOutput.Name = "txtOutput";
+            txtOutput.ScrollBars = ScrollBars.Vertical;
+            txtOutput.Size = new Size(659, 169);
+            txtOutput.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(0, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Output";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(683, 440);
+            Controls.Add(splitContainer1);
+            Controls.Add(btnClean);
+            Controls.Add(txtUrl);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "A tissue for your issue";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUrl;
+        private Button btnClean;
+        private SplitContainer splitContainer1;
+        private Label label1;
+        private TextBox txtIssue;
+        private Button btnCopy;
+        private TextBox txtOutput;
+        private Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace IssueTissue
+{
+    public partial class Form1 : Form
+    {
+        private Octokit.GitHubClient? _client;
+
+        string template = """
+
+            ### Type of issue
+
+            Other (describe below)
+
+            ### Description
+
+            {description}
+
+            ### Page URL
+
+            {pageurl}
+
+            ### Content source URL
+
+            {contentsource}
+
+            ### Document Version Independent Id
+
+            {versionid}
+
+            ### Article author
+
+            {author}
+
+            ### Metadata
+
+            {metadata}
+
+            """;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtUrl.GotFocus += TxtUrl_GotFocus;
+        }
+
+        private void TxtUrl_GotFocus(object? sender, EventArgs e)
+        {
+            txtUrl.SelectAll();
+        }
+
+        private async void btnClean_Click(object sender, EventArgs e)
+        {
+            btnCopy.Focus();
+            btnClean.Enabled = false;
+
+            // Parse URL to get issue info
+            Match match = Regex.Match(txtUrl.Text.ToLower().Trim().Replace("\\", "/"), @".*\/(.*)\/(.*)\/issues\/(.*)");
+            if (match.Groups.Count != 4)
+            {
+                MessageBox.Show("URL invalid, should be in format of 'https://github.com/{owner}/{repo}/issues/{issueID}'");
+                btnClean.Enabled = true;
+                return;
+            }
+
+            // Connect to GitHub
+            _client ??= new Octokit.GitHubClient(new Octokit.ProductHeaderValue("IssueTissue", "1.0"));
+
+            string owner;
+            string repo;
+            int issueId;
+
+            try
+            {
+                owner = match.Groups[1].Value;
+                repo = match.Groups[2].Value;
+                issueId = int.Parse(match.Groups[3].Value);
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show("Unable to parse URL correctly, possibly not a number at the end.");
+                btnClean.Enabled = true;
+                return;
+            }
+
+            // Get issue body
+            Octokit.Issue issue;
+
+            try
+            {
+                issue = await _client.Issue.Get(owner, repo, issueId);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Octokit can't get to that issue. Is the URL valid?");
+                btnClean.Enabled = true;
+                return;
+            }
+
+            txtIssue.Text = issue.Body;
+            string issueBodyTextOnly;
+
+            match = Regex.Match(txtIssue.Text, "---\\r*\\n####.*");
+
+            if (!match.Success)
+            {
+                MessageBox.Show("Unable to find metadata in issue body. Missing --- and #### headers.");
+                btnClean.Enabled = true;
+                return;
+            }
+            else
+            {
+                issueBodyTextOnly = txtIssue.Text.Substring(0, match.Index - 1);
+            }
+
+            // Find the content link in the issue body
+            match = Regex.Match(txtIssue.Text, "\\* Content: \\[.*\\]\\((.*)\\)");
+            if (!match.Success)
+            {
+                MessageBox.Show("Unable to find content link in issue body");
+                btnClean.Enabled = true;
+                return;
+            }
+
+            // Fetch content from URL
+            string contentUrl = match.Groups[1].Value;
+            using HttpClient webClient = new HttpClient();
+            HttpResponseMessage response = await webClient.GetAsync(contentUrl);
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Unable to fetch content from URL");
+                btnClean.Enabled = true;
+                return;
+            }
+
+            // Read content and parse link
+            string content = await response.Content.ReadAsStringAsync();
+            content = content.Replace("/><", "/>\n<");
+
+            // Pull out metadata from HTML head
+            MatchCollection metaMatches = Regex.Matches(content, @"<meta .+=""(.+)"" .*""(.*)"" */>");
+            Dictionary<string, string> metadata = [];
+
+            foreach (Match item in metaMatches)
+                metadata[item.Groups[1].Value] = item.Groups[2].Value;
+
+            if (metadata.Count == 0)
+            {
+                MessageBox.Show("Unable to find metadata in content");
+                btnClean.Enabled = true;
+                return;
+            }
+
+            // Check for required metadata fields
+            bool isMissingMetadataUrl = !metadata.TryGetValue("og:url", out string metaFieldUrl);
+            bool isMissingMetadataContentSource = !metadata.TryGetValue("original_content_git_url", out string metaFieldContentSource);
+            bool isMissingMetadataVersion = !metadata.TryGetValue("document_version_independent_id", out string metaFieldVersion);
+            bool isMissingMetadataAuthor = !metadata.TryGetValue("ms.author", out string metaFieldAuthor);
+            bool isMissingMetadataDocId = !metadata.TryGetValue("document_id", out string metaFieldDocId);
+
+            if (isMissingMetadataUrl
+                || isMissingMetadataContentSource
+                || isMissingMetadataVersion
+                || isMissingMetadataAuthor
+                || isMissingMetadataDocId)
+            {
+                MessageBox.Show($"""
+                    Unable to find all metadata fields in content
+                    isMissingMetadataUrl = {isMissingMetadataUrl}
+                    isMissingMetadataContentSource = {isMissingMetadataContentSource}
+                    isMissingMetadataVersion = {isMissingMetadataVersion}
+                    isMissingMetadataAuthor = {isMissingMetadataAuthor}
+                    isMissingMetadataDocId = {isMissingMetadataDocId}
+                    """);
+                btnClean.Enabled = true;
+                return;
+            }
+
+            // Fill out the template
+            string output = template
+                .Replace("{pageurl}", metaFieldUrl)
+                .Replace("{contentsource}", metaFieldContentSource)
+                .Replace("{versionid}", metaFieldVersion)
+                .Replace("{author}", metaFieldAuthor);
+
+            StringBuilder metadataStringBuilder = new();
+            metadataStringBuilder.AppendLine($"* {metadata["document_id"]}");
+            if (metadata.ContainsKey("ms.service"))
+                metadataStringBuilder.AppendLine($"* Service: **{metadata["ms.service"]}**");
+            if (metadata.ContainsKey("ms.subservice"))
+                metadataStringBuilder.AppendLine($"* Sub-service: **{metadata["ms.subservice"]}**");
+
+            txtOutput.Text = output.Replace("{metadata}", metadataStringBuilder.ToString())
+                                   .Replace("{description}", issueBodyTextOnly);
+
+            btnClean.Enabled = true;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtOutput.Text);
+            MessageBox.Show("Copied!");
+        }
+
+        private void txtUrl_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btnClean_Click(sender, e);
+            }
+        }
+    }
+}
